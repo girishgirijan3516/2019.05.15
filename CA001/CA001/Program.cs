acfc73547ecd4ef4327ccc859f16ea035841726d
Girish Vazheliparambil Girijan <738048@student.tor.loc>
2019-05-15 11:18:48 -0400

    Some updations

--- CA001/CA001/Program.cs
+++ CA001/CA001/Program.cs
@@ -13,7 +13,16 @@ namespace CA001
             // hi
             List<int> list = new List<int> {1 ,2 ,3 ,4 ,5};
             var small = GetSmallest(list);
+            var smallests = GetSmallests(list, 3);            
+
             Console.WriteLine("Small Number : {0}", small);
+
+            foreach (var number in smallests)
+            {
+                Console.WriteLine("Small Number : {0}", number);
+            }
+
+
             Console.ReadLine();
         }
         public static int GetSmallest(List<int> list)
@@ -25,6 +34,19 @@ namespace CA001
                     min = list[i];
             }
             return min;
+        }
+
+        public static List<int> GetSmallests(List<int> list, int count)
+        {
+            var smallests = new List<int>();
+
+            while (smallests.Count < count)
+            {
+                var min = GetSmallest(list);
+                smallests.Add(min);
+                list.Remove(min);
+            }
+            return smallests;
         }
     }
 }
